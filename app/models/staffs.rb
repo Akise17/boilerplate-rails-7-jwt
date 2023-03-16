8c52471a3c9a996c50be16eb48120992c5f7c25f
Liggar Prayoga <liggar@itsavirus.com>
2023-03-16 17:39:45 +0800

    remove email validations

--- app/models/staffs.rb
+++ app/models/staffs.rb
@@ -1,6 +1,5 @@
 class Staffs < ApplicationRecord
   has_many :assets
-  validates :name, :department, presence: true
-  validates :email, uniqueness: { case_sensitive: false }, presence: true, undisposable: true
+  validates :name, :email, :department, presence: true
   enum department: { hrd: 0, office_manager: 1, backend: 2, front_end: 3, qa_engineer: 4 }
 end
